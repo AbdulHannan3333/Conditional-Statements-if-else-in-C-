a4027ab639c33b59a8c0929c291e7441c97a6ae1
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:23:33 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statements__if_else_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            if (num > 0)
+            {
+                Console.WriteLine("Number is Postive");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("Number is Negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is Zero");
+            }
+        }
+    }
+}
